7be68147af01f54a6b2ef821be9cc91fd4761875
Esther*J <estherj-hsu@users.noreply.github.com>
2025-12-02 19:27:56 -0800

    tawk in global

--- js/global.js
+++ js/global.js
@@ -1,12 +1,12 @@
 // Tawk.to Script
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/5d26c0667a48df6da243fa4c/default';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/5d26c0667a48df6da243fa4c/1jbf39j12';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
 })();
 
 
